e58e1c6d5f3f23abeb18c2542cae37cec5830fd5
wali55 <walisantunu@gmail.com>
2023-08-06 00:36:17 +0600

    note app

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,64 @@
 <script setup>
-  import { ref } from "vue";
+import { ref } from "vue";
 
-  const showModal = ref(false);
-  const newNote = ref("");
+const showModal = ref(false);
+const newNote = ref("");
+const errorMessage = ref("");
+const notes = ref([]);
+
+function getRandomColor() {
+  return "hsl(" + Math.random() * 360 + ", 100%, 75%)";
+}
+
+const addNote = () => {
+  if (newNote.value.length < 10) {
+    return errorMessage.value = "Note needs to be 10 characters or more.";
+  }
+  notes.value.push({
+    id: Math.floor(Math.random() * 1000000),
+    text: newNote.value,
+    date: new Date(),
+    backgroundColor: getRandomColor(),
+  });
+
+  showModal.value = false;
+  newNote.value = "";
+  errorMessage.value = "";
+};
 </script>
 
 <template>
   <main>
     <div v-if="showModal" class="overlay">
       <div class="modal">
-        <textarea name="note" id="note" cols="30" rows="10"></textarea>
-        <button>Add Note</button>
+        <textarea
+          v-model.trim="newNote"
+          name="note"
+          id="note"
+          cols="30"
+          rows="10"
+        ></textarea>
+        <p v-if="errorMessage">{{ errorMessage }}</p>
+        <button @click="addNote">Add Note</button>
         <button class="close" @click="showModal = false">Close</button>
       </div>
     </div>
     <div class="container">
       <header>
         <h1>Notes</h1>
-        <button @click="showModal">+</button>
+        <button @click="showModal = true">+</button>
       </header>
       <div class="cards-container">
-        <div class="card">
-          <p class="main-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, quisquam!</p>
-          <p class="date">04/05/2023</p>
-        </div>
-        <div class="card">
-          <p class="main-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, quisquam!</p>
-          <p class="date">04/05/2023</p>
+        <div
+          v-for="note in notes"
+          :key="note.id"
+          class="card"
+          :style="{ backgroundColor: note.backgroundColor }"
+        >
+          <p class="main-text">
+            {{ note.text }}
+          </p>
+          <p class="date">{{ note.date.toLocaleDateString("en-US") }}</p>
         </div>
       </div>
     </div>
@@ -127,5 +159,9 @@ header button {
 .modal .close {
   background-color: rgb(204, 80, 80);
   margin-top: 10px;
+}
+
+.modal p {
+  color: rgb(204, 80, 80);
 }
 </style>
